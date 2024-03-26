c1991d33826d88166942807a56be7708cd913ae2
HUAWEI-Ubuntu <ruluy0205@163.com>
2024-03-26 19:04:21 +0800

    finish sleep

--- user/sleep.c
+++ user/sleep.c
@@ -1,8 +1,13 @@
-#include "aio.h"
-#include "alloca.h"
+#include "kernel/types.h"
+#include "user/user.h"
 
-int main(int argc, char* argv[]){
-
-
-    return 0;
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(2, "usage: sleep [ticks num]\n");
+    exit(1);
+  }
+  // atoi sys call guarantees return an integer
+  int ticks = atoi(argv[1]);
+  int ret = sleep(ticks);
+  exit(ret);
 }
